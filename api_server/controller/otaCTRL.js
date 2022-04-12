350adc93c71df8d855dfff7e97346831e01047ce
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2022-04-12 18:48:22 +0700

    test

--- api_server/controller/otaCTRL.js
+++ api_server/controller/otaCTRL.js
@@ -2,5 +2,10 @@ var path = require("path");
 
 var binPATH = path.join(process.cwd(), "binOTA/sketchVNPT.ino.esp32.bin");
 module.exports = (req, res, next) => {
+  // enable test ota
   if (req.query["u"]) res.download(binPATH);
+  else {
+    res.writeHead(400);
+    res.end("Fail");
+  }
 };
