619a958214475466e63e0a2ddd0805012ef41e31
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2022-04-16 22:00:02 +0700

    testtt

--- api_server/handler/db/storeToDB.js
+++ api_server/handler/db/storeToDB.js
@@ -1,7 +1,6 @@
 module.exports = (Model, data, query) => {
-  console.log("Test: store to db");
-  if (Model.findOne(query).then((err, data) => data))
-    Model.findOneAndUpdate(query, data);
+  console.log("Test: store to db", Model.findOne(query).exec());
+  if (Model.findOne(query).exec()) Model.findOneAndUpdate(query, data);
   else {
     var newModel = new Model(data);
     newModel.save((err, Model) => {
