b6620a9c7209140456fe8ec8a0c52f055f8cda55
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2022-04-16 22:39:07 +0700

    test

--- api_server/handler/db/storeToDB.js
+++ api_server/handler/db/storeToDB.js
@@ -2,7 +2,7 @@ module.exports = async (Model, data, query) => {
   const dataCheck = await Model.findOne(query);
   // .lean()
   // .exec((err, dataFind) => dataFind);
-  console.log("Test db:", query);
+  console.log("Test db:", dataCheck);
   // if (await dataCheck) Model.findOneAndUpdate(query, data);
   // else {
   //   var newModel = new Model(data);
