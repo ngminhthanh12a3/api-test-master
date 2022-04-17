4e3e1252373fda4d7af66e3a0a82896820574142
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2022-04-17 12:08:43 +0700

    test

--- api_server/handler/db/loadDeviceInfoFromDB.js
+++ api_server/handler/db/loadDeviceInfoFromDB.js
@@ -1,8 +1,11 @@
 module.exports = async (Model) => {
   const formatData = {};
-  const dataArray = await Model.find();
+  const dataArray = await Model.find()
+    .lean()
+    .exec((err, datas) => datas);
 
   await dataArray.forEach((data) => {
+    console.log(data);
     const { devID, _id, __v, ...props } = data;
     formatData[devID] = props;
   });
