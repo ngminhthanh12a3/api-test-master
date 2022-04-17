3aa6feeb239b48bacbe952a8c1537ec0a3c3cb22
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2022-04-17 12:26:38 +0700

    test

--- api_server/handler/device/storeToTempDeviceJSON.js
+++ api_server/handler/device/storeToTempDeviceJSON.js
@@ -2,5 +2,6 @@ const { chacha20DecryptValue } = require("../../constants");
 module.exports = (decryptJSON) => {
   console.log(decryptJSON);
   const { devID, ...propsJSON } = decryptJSON;
+  console.log(chacha20DecryptValue);
   chacha20DecryptValue[devID] = { ...propsJSON };
 };
